421b40f21d4ff0e885768bf695fc6c46fa234f36
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-05-13 21:06:43 +0300

    added owner commands

--- discord.go
+++ discord.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -41,8 +43,19 @@ func (h *DiscordHandler) messageCreate(s *discordgo.Session, m *discordgo.Messag
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
+	//Owner commands
+	if m.Author.ID == h.Bot.Creds.Owner {
+		if strings.HasPrefix(m.Content, "!vampstatus") {
+			h.StatusCmd(s, m.ChannelID)
+			h.Bot.Logger.Printf("[CMD] Sent Bot Status!")
+		}
+		if strings.HasPrefix(m.Content, "!vamplist") {
+			h.ListCmd(s, m.ChannelID)
+			h.Bot.Logger.Printf("[CMD] Sent Server List!")
+		}
+	}
 	//Admin commands
-	if admin, _ := IsAdmin(s, m); admin {
+	if admin, _ := IsAdmin(s, m); admin || m.Author.ID == h.Bot.Creds.Owner {
 		if strings.HasPrefix(m.Content, "!setvamp") {
 			ch, _ := h.Bot.Database.CreateChan(m.ChannelID, "!")
 			h.Bot.Database.Chans[m.ChannelID] = ch
@@ -87,4 +100,69 @@ func IsAdmin(s *discordgo.Session, m *discordgo.MessageCreate) (bool, error) {
 		return false, err
 	}
 	return perm&discordgo.PermissionAdministrator != 0, nil
+}
+
+//Bot status command
+func (h *DiscordHandler) StatusCmd(s *discordgo.Session, c string) {
+	guilds := s.State.Guilds
+	m := discordgo.MessageEmbed{
+		Title:       "Vampire Survivors Bot Status",
+		Color:       12846604,
+		Description: "This bot is free and open source. \nCheck the [GitHub](https://github.com/SHA65536/Vampire-Survivors-Bot)",
+		Fields: []*discordgo.MessageEmbedField{
+			{Name: "Connected Servers", Value: fmt.Sprint(len(guilds))},
+			{Name: "Number Of Library Entries", Value: fmt.Sprint(len(h.Bot.Library.DLibrary))},
+		},
+		Footer: &discordgo.MessageEmbedFooter{
+			IconURL: "https://ucarecdn.com/dc6980a3-c7d4-4164-9b02-276ea7832791/BotIcon.png",
+			Text:    "Vampire Bot @ github.com/SHA65536/Vampire-Survivors-Bot",
+		},
+		Thumbnail: &discordgo.MessageEmbedThumbnail{
+			URL: "https://ucarecdn.com/0bb2dc9b-f74e-4d1a-a106-af81331cb5cd/Bat.gif",
+		},
+	}
+	err := SendEmbed(s, c, m)
+	if err != nil {
+		h.Bot.Logger.Print(err)
+	}
+}
+
+//Bot status command
+func (h *DiscordHandler) ListCmd(s *discordgo.Session, c string) {
+	guilds := s.State.Guilds
+	sort.Slice(guilds, func(i, j int) bool {
+		return guilds[i].MemberCount > guilds[j].MemberCount
+	})
+	fields := []*discordgo.MessageEmbedField{}
+	curf := &discordgo.MessageEmbedField{Name: "Members - Name - ID"}
+	informations := []string{}
+	for _, g := range guilds {
+		last_for_id := g.ID[len(g.ID)-4:]
+		informations = append(informations, fmt.Sprintf("%d - %s - %s\n", g.MemberCount, g.Name, last_for_id))
+	}
+	for _, msg := range informations {
+		if len(curf.Value)+len(msg) >= 1024 {
+			fields = append(fields, curf)
+			curf = &discordgo.MessageEmbedField{Name: "Members - Name - ID"}
+		}
+		curf.Value += msg
+	}
+	fields = append(fields, curf)
+	m := discordgo.MessageEmbed{
+		Title:       "Vampire Survivors Server List",
+		Color:       12846604,
+		Description: "This bot is free and open source. \nCheck the [GitHub](https://github.com/SHA65536/Vampire-Survivors-Bot)",
+		Fields:      fields,
+		Footer: &discordgo.MessageEmbedFooter{
+			IconURL: "https://ucarecdn.com/dc6980a3-c7d4-4164-9b02-276ea7832791/BotIcon.png",
+			Text:    "Vampire Bot @ github.com/SHA65536/Vampire-Survivors-Bot",
+		},
+		Thumbnail: &discordgo.MessageEmbedThumbnail{
+			URL: "https://ucarecdn.com/0bb2dc9b-f74e-4d1a-a106-af81331cb5cd/Bat.gif",
+		},
+	}
+	err := SendEmbed(s, c, m)
+	if err != nil {
+		h.Bot.Logger.Print(err)
+	}
 }
